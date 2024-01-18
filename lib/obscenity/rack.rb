f2f1040f77fb077f6436356745327005b2667a13
João Pedro <hi@joaonetto.me>
2024-01-18 13:34:55 -0300

    changing File.exists? to File.exist? to make it work on ruby 3

--- lib/obscenity/rack.rb
+++ lib/obscenity/rack.rb
@@ -1,42 +1,42 @@
 module Rack
   class Obscenity
-        
-    def initialize(app, options = {})  
+
+    def initialize(app, options = {})
       @app, @options = app, options
-    end  
-    
+    end
+
     def call(env)
       rejectable  = false
       post_params = Rack::Utils.parse_query(env['rack.input'].read, "&")
       get_params  = Rack::Utils.parse_query(env['QUERY_STRING'],    "&")
-      
+
       if @options.has_key?(:reject)
         rejactable = validate_rejectability_of( select_params(:reject, get_params.update(post_params)) )
-      
+
       elsif @options.has_key?(:sanitize)
         get_params  = sanitize_contents_of(get_params)
         post_params = sanitize_contents_of(post_params)
-        
+
         env['QUERY_STRING'] = Rack::Utils.build_query(get_params)
         env['rack.input']   = StringIO.new(Rack::Utils.build_query(post_params))
       end
-      
+
       rejactable ? reject : continue(env)
     end
-    
+
     private
     def continue(env)
       @app.call(env)
     end
-    
+
     def reject
       length, content = 0, ''
-      if @options[:reject].is_a?(Hash) 
+      if @options[:reject].is_a?(Hash)
         if (message = @options[:reject][:message]).present?
           content = message
           length  = message.size
         elsif (path = @options[:reject][:path]).present?
-          if (path = ::File.expand_path(path)) && ::File.exists?(path)
+          if (path = ::File.expand_path(path)) && ::File.exist?(path)
             content = ::File.read(path)
             length  = content.size
           end
@@ -63,7 +63,7 @@ module Rack
       end
       should_reject_request
     end
-        
+
     def sanitize_contents_of(params)
       sanitized_params   = {}
       replacement_method = @options[:sanitize].is_a?(Hash) && @options[:sanitize][:replacement]
@@ -77,10 +77,10 @@ module Rack
       }
       params.update(sanitized_params)
     end
-    
+
     def select_params(key, params = {})
       if @options[key].is_a?(Hash) && @options[key][:params].is_a?(Array)
-        params.select{ |param, vvalue| @options[key][:params].include?(param.to_sym) } 
+        params.select{ |param, vvalue| @options[key][:params].include?(param.to_sym) }
       else
         params
       end
